1af636a8a0f180a84a206f4964124cbb0de698d4
Mateusz <msikorski@griddynamics.com>
2024-11-08 10:43:06 +0100

    Added transactional read only annotations.

--- catalog/src/main/java/com/github/matsik/catalog/product/ProductService.java
+++ catalog/src/main/java/com/github/matsik/catalog/product/ProductService.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
@@ -13,11 +14,13 @@ public class ProductService {
 
     private final ProductRepository productRepository;
 
+    @Transactional(readOnly = true)
     public Product findById(String id) {
         Optional<Product> product = productRepository.findById(id);
         return product.orElseThrow(() -> new RuntimeException(String.format("Product with: %s does not exist", id)));
     }
 
+    @Transactional(readOnly = true)
     public Page<Product> findAllBySku(PageRequest pageRequest) {
         return productRepository.findAll(pageRequest);
     }
