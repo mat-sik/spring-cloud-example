bb0cd0549c4f65a7ef83e677a8fb48b913b70ba6
Mateusz <msikorski@griddynamics.com>
2024-11-08 10:42:40 +0100

    Implement code for loading entities from csv file.

--- catalog/src/main/java/com/github/matsik/catalog/product/Product.java
+++ catalog/src/main/java/com/github/matsik/catalog/product/Product.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -16,6 +17,7 @@ import java.math.BigDecimal;
 @AllArgsConstructor
 @Getter
 @Setter
+@Builder
 public class Product {
 
     @Id
